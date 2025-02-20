2f3090c1a845d50d1bfd75fe2ac5f179f23603f6
yogurtshake <59886219+yogurtshake@users.noreply.github.com>
2025-02-19 21:08:42 -0500

    added mp3 functionality to !s

--- bot.py
+++ bot.py
@@ -181,15 +181,21 @@ async def s(ctx, *name):
         return
     
     sound_path = SOUNDS_FOLDER_PATH + SLASH + input + ".ogg"
+    sound_path_mp3 = SOUNDS_FOLDER_PATH + SLASH + input + ".mp3"
     basename = sound_path.split('/')[-1].strip()
+    basename_mp3 = sound_path_mp3.split('/')[-1].strip()
 
     if not basename.lower() in SOUNDS:
-        await ctx.send(f"*'{basename}' does not exist. You piece of shit.*")
-        return
-    elif not os.path.exists(sound_path) and not WINDOWS:
+        if not basename_mp3.lower() in SOUNDS:
+            await ctx.send(f"*Sound `{basename[:-4]}` does not exist. You piece of shit.*")
+            return
+
+        sound_path = sound_path_mp3
+        basename = basename_mp3
+        
+    if not os.path.exists(sound_path) and not WINDOWS:
         await ctx.send(f"*'{basename}' is missing CAPS somewhere. You piece of shit.*")
         return
-
 
     ctx.voice_client.stop()
     ctx.voice_client.play(discord.FFmpegPCMAudio(sound_path), after=lambda e: print(f'Finished playing: {basename}'))
